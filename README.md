9136488e8b4850a4a86240c77c2b2668530e11c8
sangtrandev00 <nhatsang0101@gmail.com>
2023-09-19 15:00:16 +0700

    First commit Angular NG API

---
+++ README.md
@@ -0,0 +1,5 @@
+# RESTFUL API TECHSPACE B2B - DOCS
+This document provides information about the RESTful API endpoints implemented in the Node.js code you provided. The API allows you to perform CRUD operations (Create, Read, Update, Delete,...) on categories, products, users, orders, auth.
+## Base URL
+
+https://fullstack-techspace-es6-api.onrender.com 
---
+++ routes/adminProduct.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const adminProductsController = require("../controllers/adminProducts");
+const uploadMiddleware = require("../middleware/upload");
+const isAuth = require("../middleware/is-auth");
+const router = express.Router();
+const { check, body } = require("express-validator");
+
+// GET PRODUCTS
+router.get("/products", adminProductsController.getProducts);
+
+// GET BY RANGES[MIN, MAX];
+
+// router.get("/products-by-price-range", adminProductsController.getProductsInRange);
+router.get("/random-products", adminProductsController.createRandomProducts);
+
+// GET PRODUCT
+
+router.get("/products/:productId", adminProductsController.getProduct);
+
+// POST PRODUCT
+router.post("/product", adminProductsController.postProduct);
+
+// PUT PRODUCT
+router.put("/product/:productId", adminProductsController.updateProduct);
+
+// DELETE PRODUCT
+router.delete("/products/:productId", adminProductsController.deleteProduct);
+
+module.exports = router;
